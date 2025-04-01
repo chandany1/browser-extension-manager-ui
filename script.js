d60510589b13a180f044da62e539f8ad26f512bf
chandany1 <chandanyadav57@gmail.com>
2025-04-01 21:38:25 +0530

    updated theme

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('helllo');
+const themeIcon = document.querySelector('.theme-icon');
+const body = document.body;
+const menuItems = document.querySelectorAll("#menu li");
+
+
+themeIcon.addEventListener('click',function(){
+    if (body.classList.contains("light-theme")) {
+        body.classList.replace("light-theme", "dark-theme");
+        this.src = './assets/images/icon-sun.svg';
+      } else {
+        body.classList.replace("dark-theme", "light-theme");
+        this.src = './assets/images/icon-moon.svg';
+      }
+});
+
+menuItems.forEach(item => {
+  item.addEventListener("click", function() {
+    menuItems.forEach(li => li.classList.remove("active"));
+      this.classList.add("active");
+  });
+});
